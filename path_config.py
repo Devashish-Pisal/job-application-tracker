7431457235cdf300fcb5791568929fb00260f7ad
Devashish-Pisal <dpisal85@gmail.com>
2026-05-02 22:09:58 +0200

    add path for fetched emails directory

--- path_config.py
+++ path_config.py
@@ -6,12 +6,14 @@ PROJECT_ROOT = Path(__file__).parent
 DATA_DIR_PATH = PROJECT_ROOT / "data"
 PROMPTS_DIR_PATH = DATA_DIR_PATH / "prompts"
 MANUAL_CHECK_DIR = DATA_DIR_PATH / "manual_check"
+FETCHED_EMAILS = DATA_DIR_PATH / "fetched_emails"
 
 
 # Files
 CREDENTIALS_FILE_PATH = DATA_DIR_PATH / "credentials.json"
 TOKEN_FILE_PATH = DATA_DIR_PATH / "token.json"
 DUP_BY_MSG_ID_FILE_PATH = MANUAL_CHECK_DIR / "duplicate_by_msg_id.jsonl"
-DUP_BY_DEDUP_KEY_FILE_PATH = MANUAL_CHECK_DIR / "duplicate_by_dedup_key.jsonl"
 LLM_LOW_CONF_OUTPUT_FILE = MANUAL_CHECK_DIR / "llm_low_confidence_output.jsonl"
+DUP_BY_FUZZY_MATCH = MANUAL_CHECK_DIR / "duplicate_by_fuzzy_matching.jsonl"
+
 
