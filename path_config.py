bef75fa7419db8ad0ef2318460fbb434bf7870ce
Devashish-Pisal <dpisal85@gmail.com>
2026-05-03 17:40:34 +0200

    add list of all paths

--- path_config.py
+++ path_config.py
@@ -7,7 +7,7 @@ DATA_DIR_PATH = PROJECT_ROOT / "data"
 PROMPTS_DIR_PATH = DATA_DIR_PATH / "prompts"
 MANUAL_CHECK_DIR = DATA_DIR_PATH / "manual_check"
 FETCHED_EMAILS = DATA_DIR_PATH / "fetched_emails"
-
+LOGS_DIR_PATH = DATA_DIR_PATH / "logs"
 
 # Files
 CREDENTIALS_FILE_PATH = DATA_DIR_PATH / "credentials.json"
@@ -17,3 +17,8 @@ LLM_LOW_CONF_OUTPUT_FILE = MANUAL_CHECK_DIR / "llm_low_confidence_output.jsonl"
 DUP_BY_FUZZY_MATCH = MANUAL_CHECK_DIR / "duplicate_by_fuzzy_matching.jsonl"
 
 
+# List of all paths to create the folders and files dynamically at once before starting application
+ALL_PATHS = [
+    DATA_DIR_PATH, PROMPTS_DIR_PATH, MANUAL_CHECK_DIR, FETCHED_EMAILS, LOGS_DIR_PATH,
+    CREDENTIALS_FILE_PATH, TOKEN_FILE_PATH, DUP_BY_MSG_ID_FILE_PATH, LLM_LOW_CONF_OUTPUT_FILE, DUP_BY_FUZZY_MATCH
+]
