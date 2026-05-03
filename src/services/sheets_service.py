bc7a2a67ddf31d929efa41c772784312ac28cddd
Devashish-Pisal <dpisal85@gmail.com>
2026-05-03 18:51:03 +0200

    implement multiple new functions

--- src/services/sheets_service.py
+++ src/services/sheets_service.py
@@ -1,6 +1,11 @@
+import time
+from src.config import SHEET_COLUMN_NAME_INDEX_MAPPING
 from googleapiclient.discovery import build
 from loguru import logger
-from rapidfuzz import fuzz
+from rapidfuzz import fuzz, process
+from datetime import datetime
+import json
+
 
 def get_sheets_service(creds):
     return build("sheets", "v4", credentials=creds)
@@ -8,68 +13,124 @@ def get_sheets_service(creds):
 
 def append_row(service, sheet_id, data):
     values = [[
-        data["date"], # column A
-        data["company"], # column B
+        data["application_date"], # column A
+        data["company_name"], # column B
         data["role"], # column C
-        data["status"], # column D
-        data["source"], # column E
-        data["confidence"], # column F
-        data["email_body"], # column G
-        data["status_flow"], # column H
+        data["current_status"], # column D
+        data["current_confidence"], # column E
+        data["status_flow"], # column F
+        data["history"], # column G
+        data["last_row_modification_date"], # column H
         data["message_id"], #  column I
-        data["dedup_key"] # (company_role )  column J
     ]]
     body = {"values": values}
+    time.sleep(1) # Sleep for 1 sec to avoid "Too many requrests" error
     service.spreadsheets().values().append(
         spreadsheetId=sheet_id,
-        range="Sheet1!A:J",
+        range="Sheet1!A:I",
         valueInputOption="RAW",
         body=body
     ).execute()
-    logger.info(f"Row appended to spreadsheet: {values}")
+    logger.success(f"Row appended to spreadsheet: {values}")
 
 
-def modify_row(service, sheet_id, row_index, new_data):
-    # TODO
+def modify_row(service, sheet_id, row_index, llm_output, email_data):
+    time.sleep(1)
+    row_data = service.spreadsheets().values().get(
+        spreadsheetId=sheet_id,
+        range=f"Sheet1!{row_index}:{row_index}"
+    ).execute()
+    values = row_data.get("values", [])[0]
+    updated_row = prepare_row_modification_data(llm_output, email_data, values)
+    time.sleep(1)
     service.spreadsheets().values().update(
         spreadsheetId=sheet_id,
-        range="Sheet1!A5:D5",  # row 5
-        valueInputOption="USER_ENTERED",
+        range=f"Sheet1!A{row_index}:I{row_index}",
+        valueInputOption="RAW",
         body={
-            "values": [[
-                "new_company",
-                "new_title",
-                "new_status",
-                "new_dedup_key"
-            ]]
+            "values": updated_row
         }
     ).execute()
-    pass
+    logger.success(f"Row at index {row_index} successfully modified with values {updated_row}")
+
 
 def message_exists_by_id(service, sheet_id, message_id):
+    time.sleep(1)
     result = service.spreadsheets().values().get(
         spreadsheetId=sheet_id,
-        range="Sheet1!I:I"
+        range=f"Sheet1!{SHEET_COLUMN_NAME_INDEX_MAPPING["message-ids"]}:{SHEET_COLUMN_NAME_INDEX_MAPPING["message-ids"]}"
     ).execute()
     values = result.get("values", [])
-    for row in values:
-        if row and row[0] == message_id:
-            return True
-    return False
+    ids = {
+        single_id.strip()
+        for row in values if row
+        for single_id in row[0].split(",")
+    }
+    return message_id in ids
 
 
-def message_exists_by_dedup_key(service, sheet_id, dedup_key):
+def fuzzy_match_column_values(service, sheet_id, column_index:str, input_to_match:str, threshold:float):
+    time.sleep(1)
     result = service.spreadsheets().values().get(
         spreadsheetId=sheet_id,
-        range="Sheet1!J:J"
+        range=f"Sheet1!{column_index}:{column_index}"
     ).execute()
     values = result.get("values", [])
-    for idx, row in enumerate(values):
-        if not row or not row[0] or idx == 0:
+    matches = []
+    input_to_match = (input_to_match or "").strip().lower()
+    for idx, row in enumerate(values[1:], start=2):
+        if not row:
             continue
-        if is_similar_dedup_key(dedup_key, row[0]):
-            return True
-    return False
+        value = row[0].strip().lower()
+        if value in ("", "null", "n/a"):
+            continue
+        score = fuzz.token_set_ratio(input_to_match, value) / 100
+        if score >= threshold:
+            matches.append({
+                "row_index": idx, # index == actual row number
+                "value": value,
+                "score": score,
+            })
+    return len(matches), matches
+
+
+def fuzzy_match_company_and_role(service, sheet_id, company_col_index, role_col_index, company_name, role, threshold):
+    time.sleep(1)
+    company_res = service.spreadsheets().values().get(
+        spreadsheetId=sheet_id,
+        range=f"Sheet1!{company_col_index}:{company_col_index}"
+    ).execute()
+    time.sleep(1)
+    role_res = service.spreadsheets().values().get(
+        spreadsheetId=sheet_id,
+        range=f"Sheet1!{role_col_index}:{role_col_index}"
+    ).execute()
+    company_col = company_res.get("values", [])
+    role_col = role_res.get("values", [])
+    matches = []
+    for idx, (c_name, r_name) in enumerate(zip(company_col[1:], role_col[1:]), start=2):
+        c_name = c_name[0].lower() if c_name else ""
+        r_name = r_name[0].lower() if r_name else ""
+        c_score = fuzz.token_set_ratio(company_name, c_name) / 100
+        r_score = fuzz.token_set_ratio(role, r_name) / 100
+        final_score = c_score * 0.6 + r_score * 0.4
+        if final_score >= threshold:
+            matches.append({
+                "row_index": idx, # index == actual row number
+                "company_name": c_name,
+                "role_name": r_name,
+                "score": final_score,
+            })
+    return len(matches), matches
+
+
+def get_all_rows(service, sheet_id):
+    time.sleep(1)
+    result = service.spreadsheets().values().get(
+        spreadsheetId=sheet_id,
+        range="Sheet1!A:J"
+    ).execute()
+    return result.get("values", [])
 
 
 def is_similar_dedup_key(key1: str, key2: str, threshold=85):
@@ -80,3 +141,76 @@ def is_similar_dedup_key(key1: str, key2: str, threshold=85):
     # weight company higher (more important)
     final_score = (0.6 * company_score) + (0.4 * title_score)
     return final_score >= threshold
+
+
+def prepare_new_row_data(llm_output, email_data):
+    history = prepare_history_to_append(llm_output, email_data)
+    company_name = llm_output["normalized_company_name"] if llm_output.get("normalized_company_name") and llm_output.get("normalized_company_name") != "null" else "N/A"
+    role = llm_output["normalized_job_title"]  if llm_output.get("normalized_job_title") and llm_output.get("normalized_job_title") != "null" else "n/a"
+    return{
+        "application_date": email_data.get("date", "N/A"),
+        "company_name": company_name.strip().upper(),
+        "role": role.strip().lower(),
+        "current_status": llm_output.get("email_type", "N/A"),
+        "current_confidence": llm_output.get("confidence", "N/A"),
+        "status_flow": llm_output.get("email_type", "N/A"),
+        "history":  history if history else "N/A",
+        "last_row_modification_date": datetime.now().strftime("%Y-%m-%d"),
+        "message_id": email_data.get("id", "N/A")
+    }
+
+
+
+def prepare_row_modification_data(llm_output, email_data, existing_row):
+    modified_row = [existing_row[0]]
+    # Fix company name (if not available)
+    if existing_row[1] == "N/A" and llm_output["normalized_company_name"] and llm_output["normalized_company_name"] != "null":
+        modified_row.append(llm_output.get("normalized_company_name", "N/A").strip().upper())
+    else:
+        modified_row.append(existing_row[1])
+    # Fix role (if not available)
+    if existing_row[2] == "n/a" and llm_output["normalized_job_title"] and llm_output["normalized_job_title"] != "null":
+        modified_row.append(llm_output.get("normalized_job_title", "n/a").strip().lower())
+    else:
+        modified_row.append(existing_row[2])
+    # take current status as it is
+    modified_row.append(llm_output.get("email_type", "N/A"))
+    # take current confidence as it is
+    modified_row.append(llm_output.get("confidence", "N/A"))
+    # update status flow
+    new_status_flow = existing_row[5] + " ⟶ " + llm_output["email_type"]
+    modified_row.append(new_status_flow)
+    # update history
+    new_history = existing_row[6] + prepare_history_to_append(llm_output, email_data)
+    modified_row.append(new_history)
+    # add last row modification date
+    modified_row.append(datetime.now().strftime("%Y-%m-%d"))
+    # append new email id
+    new_ids = existing_row[8] + ", " + email_data.get('id')
+    modified_row.append(new_ids)
+    return [modified_row]
+
+
+def prepare_history_to_append(llm_output, email_data):
+    formatted_llm = json.dumps(llm_output, indent=2, ensure_ascii=False)
+    history_entry = f"""--- NEW EMAIL ENTRY ---
+date appended: {datetime.now().strftime("%Y-%m-%d")}
+
+llm_output:
+{formatted_llm}
+
+email metadata:
+  message id: {email_data.get("id")}
+  date received: {email_data.get("date")}
+  sender name: {email_data.get("sender_name")}
+  sender email: {email_data.get("sender_email")}
+
+subject:
+{email_data.get("subject")}
+
+body:
+{email_data.get("body")}
+
+=================================================================================================================================
+"""
+    return history_entry
