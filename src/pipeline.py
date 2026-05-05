c4aab3622f0e7edb8445750b647f105f261323db
Devashish-Pisal <dpisal85@gmail.com>
2026-05-05 14:47:04 +0200

    pass status column and current status for fuzzy matching

--- src/pipeline.py
+++ src/pipeline.py
@@ -5,8 +5,9 @@ from src.services.parser_service import parse_email
 from src.llm.gemini import get_gemini_client, extract_job_info
 from src.config import SHEET_ID, GEMINI_CONFIG, SHEET_COLUMN_NAME_INDEX_MAPPING
 from path_config import  PROMPTS_DIR_PATH, DUP_BY_MSG_ID_FILE_PATH, DUP_BY_FUZZY_MATCH, LLM_LOW_CONF_OUTPUT_FILE, FETCHED_EMAILS
-from src.utils.helpers import load_raw_prompt, convert_email_dict_to_text, append_jsonl, get_gmail_and_sheet_services, construct_logging_object, delete_files
 from src.services.sheets_service import append_row, message_exists_by_id, fuzzy_match_column_values, modify_row, fuzzy_match_company_and_role, prepare_new_row_data
+from src.utils.helpers import load_raw_prompt, convert_email_dict_to_text, append_jsonl, get_gmail_and_sheet_services, construct_logging_object, delete_files, normalize_umlauts
+
 
 
 
@@ -15,12 +16,14 @@ def process_high_conf_output(sheet_service,llm_output, email_data, logging_obj):
     role_fuzzy_threshold = 0.85
     company_name_col_index = SHEET_COLUMN_NAME_INDEX_MAPPING["company-name"]
     role_col_index = SHEET_COLUMN_NAME_INDEX_MAPPING["role"]
+    status_col_index = SHEET_COLUMN_NAME_INDEX_MAPPING["current-status"]
     company_name = llm_output["normalized_company_name"].strip().upper()
     role = llm_output["normalized_job_title"].strip().lower()
+    current_status = llm_output["email_type"].strip().upper()
     new_entry = prepare_new_row_data(llm_output=llm_output, email_data=email_data)
 
     if company_name and role and company_name != "NULL" and role != "null": # Company name - Present; Role - Present
-        count, matches = fuzzy_match_company_and_role(sheet_service, SHEET_ID, company_name_col_index, role_col_index, company_name, role, threshold=0.87)
+        count, matches = fuzzy_match_company_and_role(sheet_service, SHEET_ID, company_name_col_index, role_col_index, status_col_index, company_name, role, current_status, threshold=0.87)
         if count == 0:
             append_row(service=sheet_service, sheet_id=SHEET_ID, data=new_entry)
         elif count == 1:
@@ -30,7 +33,7 @@ def process_high_conf_output(sheet_service,llm_output, email_data, logging_obj):
             append_jsonl(DUP_BY_FUZZY_MATCH, logging_obj)
 
     elif company_name and company_name != "NULL" and (not role or role == "null"): # Company name - Present; Role - Absent
-        count, matches = fuzzy_match_column_values(sheet_service, SHEET_ID, company_name_col_index, company_name, company_fuzzy_threshold)
+        count, matches = fuzzy_match_column_values(sheet_service, SHEET_ID, company_name_col_index, status_col_index, company_name, current_status, company_fuzzy_threshold)
         if count == 0:
             append_row(sheet_service, SHEET_ID, new_entry)
         elif count == 1:
@@ -40,7 +43,7 @@ def process_high_conf_output(sheet_service,llm_output, email_data, logging_obj):
             append_jsonl(DUP_BY_FUZZY_MATCH, logging_obj)
 
     elif (not company_name or company_name == "NULL") and role and role != "null": # Company name - Absent; Role - Present
-        count, matches = fuzzy_match_column_values(sheet_service, role_col_index, role, role_fuzzy_threshold)
+        count, matches = fuzzy_match_column_values(sheet_service, SHEET_ID, role_col_index, status_col_index, role, current_status, role_fuzzy_threshold)
         if count == 0:
             append_row(sheet_service, SHEET_ID, new_entry)
         elif count == 1:
@@ -82,6 +85,7 @@ def pipeline():
                             max_retries=GEMINI_CONFIG["max_retries"],
                             temp=GEMINI_CONFIG["temp"]
                         )
+                        llm_output = normalize_umlauts(llm_output)
                         logging_obj = construct_logging_object(llm_output, email_data)
                         if float(llm_output["confidence"]) >= 0.70:
                             process_high_conf_output(sheet_service, llm_output, email_data, logging_obj)
