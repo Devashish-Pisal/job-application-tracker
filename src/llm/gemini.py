aad696e6bfdde1f0f8b873b4836e91413f1dcaf4
Devashish-Pisal <dpisal85@gmail.com>
2026-05-16 20:02:42 +0200

    implement multiple model support

--- src/llm/gemini.py
+++ src/llm/gemini.py
@@ -29,29 +29,31 @@ JOB_SCHEMA = {
     ]
 }
 
-def extract_job_info(client, system_prompt, email_data, model="gemini-2.5-flash", max_retries=3, temp=0.2):
+def extract_job_info(client, system_prompt, email_data, models, max_retries=3, temp=0.1):
     last_error = None
-    for attempt in range(1, max_retries + 1):
-        try:
-            response = client.models.generate_content(
-                model=model,
-                contents=[
-                    system_prompt,
-                    f"Input Email data:\n{email_data}"
-                ],
-                config={
-                    "temperature": temp,
-                    "response_mime_type": "application/json",
-                    "response_schema": JOB_SCHEMA
-                }
-            )
-            if isinstance(response.parsed, dict):
-                return response.parsed
-            else:
-                return json.loads(response.text)
-        except Exception as e:
-            last_error = e
-            wait = (2 ** attempt) + random.uniform(0, 1)
-            logger.warning(f"[Retry {attempt}] Error: {e} | waiting {wait:.2f}s")
-            time.sleep(wait)
-    raise RuntimeError(f"Failed after {max_retries} retries. Last error: {last_error}")
+    for model in models:
+        for attempt in range(1, max_retries + 1):
+            try:
+                response = client.models.generate_content(
+                    model=model,
+                    contents=[
+                        system_prompt,
+                        f"Input Email data:\n{email_data}"
+                    ],
+                    config={
+                        "temperature": temp,
+                        "response_mime_type": "application/json",
+                        "response_schema": JOB_SCHEMA
+                    }
+                )
+                if isinstance(response.parsed, dict):
+                    return response.parsed
+                else:
+                    return json.loads(response.text)
+            except Exception as e:
+                last_error = e
+                wait = (2 ** attempt) + random.uniform(0, 1)
+                logger.warning(f"['{model}' retry {attempt}] Error: {e} | waiting {wait:.2f}s")
+                time.sleep(wait)
+        logger.warning(f"'{model}' failed after {max_retries} retries. Last error: {last_error}")
+    raise RuntimeError(f"All models in list {models} failed")
